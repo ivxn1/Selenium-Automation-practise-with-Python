59b7c2ce4dc3b5d051dd5481e7c233087c49290d
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-01-18 14:27:11 +0200

    Updated project

--- End_to_End_Automation/Library/ConfigReader.py
+++ End_to_End_Automation/Library/ConfigReader.py
@@ -4,3 +4,8 @@ def read_config(section, key):
     config = configparser.ConfigParser()
     config.read('../ConfigurationFiles/Config.cfg')
     return config.get(section, key)
+
+def get_element_locator(section, key):
+    config = configparser.ConfigParser()
+    config.read('../ConfigurationFiles/Elements.cfg')
+    return config.get(section, key)
--- End_to_End_Automation/TestCases/test_TestCase1_Registration_Form.py
+++ End_to_End_Automation/TestCases/test_TestCase1_Registration_Form.py
@@ -1,12 +1,15 @@
 from selenium.webdriver.common.by import By
 from Selenium_Automation.End_to_End_Automation.Base.base import open_browser, close_browser
+from Selenium_Automation.End_to_End_Automation.Library import ConfigReader
 
 def test_Validate_Registration():
 
-    chrome = open_browser()
-    chrome.find_element(by=By.XPATH, value='//*[@id="tab-content1"]/form/input[2]').send_keys('ivan')
-    chrome.find_element(by=By.XPATH, value='//*[@id="tab-content1"]/form/input[3]').send_keys('abc@abc.com')
-    chrome.find_element(by=By.XPATH, value='//*[@id="tab-content1"]/form/input[4]').send_keys('pass123')
-    chrome.find_element(by=By.XPATH, value='//*[@id="tab-content1"]/form/input[5]').send_keys('pass123')
+    browser = open_browser()
+
+    browser.find_element(by=By.XPATH, value=ConfigReader.get_element_locator('Registration', 'username_field')).send_keys('ivan')
+    browser.find_element(by=By.XPATH, value=ConfigReader.get_element_locator('Registration', 'email_field')).send_keys('ivan@ivan.com')
+    browser.find_element(by=By.XPATH, value=ConfigReader.get_element_locator('Registration', 'password_field')).send_keys('123')
+    browser.find_element(by=By.XPATH, value=ConfigReader.get_element_locator('Registration', 'confirm_password_field')).send_keys('123')
+
     close_browser()
 
